e52d6bf09525f9ab4dedd61802af4ea0088e8ad6
Pavan Gudimetta <pgudimetta@gmail.com>
2017-02-08 21:24:41 -0500

    updated db rules

---
+++ canihazfirewalld/firewallMe.py
@@ -0,0 +1,12 @@
+# firewallMe.py
+# Instructional Programming on Linux IPTables  Firewalls
+#
+#
+#!/bin/env python
+import os
+print("FirewallMe - Simple introduction to Linux Firewalls")
+print("Choose From One of the Following Options:")
+print("1 - Show Active Firewall Rules")
+print("2 - show ")
+print("3 - Show Man Page for IPTables")
+print("4 - Show this Help and Exit")
